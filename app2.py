c6003ee23ff1b68528cc221da4be1981c2f67b81
Tamatimtam <pratama.varian.andika.parulian.tik22@mhsw.pnj.ac.id>
2025-02-05 16:57:10 +0700

    added cloudbuildyml

--- app2.py
+++ app2.py
@@ -12,7 +12,7 @@ app.secret_key = secrets.token_hex(16)  # This creates a 32-character random hex
 port = int(os.environ.get('PORT', 4443))
 
 #INIT FIREBASE
-cred = credentials.Certificate("codenameamber-7b92a-firebase-adminsdk-fbsvc-91917bcd0a.json")
+cred = credentials.Certificate("firebase-credentials.json")
 firebase_admin.initialize_app(cred)
 
 def isloggedin(f):
