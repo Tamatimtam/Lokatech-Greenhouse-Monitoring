adb4fc1fd6766d65e0613420b19da5bbf8bfdd7e
timtam <pratama.varian.andika.parulian.tik22@mhsw.pnj.ac.id>
2025-04-05 20:28:41 +0700

    Clean up duplicate hardware files after merge

--- lib/Common/NodeConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef NODECONFIG_H
-#define NODECONFIG_H
-
-// Common hardware pin definitions
-#define DHT_PIN 4  // DHT22 data pin connected to GPIO4 (Assuming it's the same for all nodes using it)
-
-// Add other common configuration constants here as needed
-
-#endif // NODECONFIG_H
