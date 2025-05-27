0e009209f4bfb701fb5c7977ea17736ada3830f3
timtam <pratama.varian.andika.parulian.tik22@mhsw.pnj.ac.id>
2025-05-27 12:38:00 +0700

    TEMPORARY change frontside API key to backup keys

--- static/js/firebase-init.js
+++ static/js/firebase-init.js
@@ -1,13 +1,35 @@
-// Firebase configuration
+// // Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC2-95FPg6b7vOXJQxbXOlUXEb05ETJTwk",
+//     authDomain: "codenameamber-7b92a.firebaseapp.com",
+//     projectId: "codenameamber-7b92a",
+//     storageBucket: "codenameamber-7b92a.firebasestorage.app",
+//     messagingSenderId: "948347816183",
+//     appId: "1:948347816183:web:e5e1514e3b4c752dbd2bf2",
+//     measurementId: "G-0LD4X3RQZ5"
+// };
+
+
+// BACKUP CONFIGS
 const firebaseConfig = {
-    apiKey: "AIzaSyC2-95FPg6b7vOXJQxbXOlUXEb05ETJTwk",
-    authDomain: "codenameamber-7b92a.firebaseapp.com",
-    projectId: "codenameamber-7b92a",
-    storageBucket: "codenameamber-7b92a.firebasestorage.app",
-    messagingSenderId: "948347816183",
-    appId: "1:948347816183:web:e5e1514e3b4c752dbd2bf2",
-    measurementId: "G-0LD4X3RQZ5"
+
+  apiKey: "AIzaSyDL5lgdZgKDEXwmPmaf8JWDxefTzu2uqKY",
+
+  authDomain: "codename-emerald.firebaseapp.com",
+
+  projectId: "codename-emerald",
+
+  storageBucket: "codename-emerald.firebasestorage.app",
+
+  messagingSenderId: "67061553192",
+
+  appId: "1:67061553192:web:015b2ce3c32e02c4a1fd1d"
+
 };
+
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
